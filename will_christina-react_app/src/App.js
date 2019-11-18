f70e760e96b011aafdc9de9bf752c593a146e0d8
Christina Chapman <cmchapman75@gmail.com>
2019-11-18 14:30:55 -0600

    working on filter

--- will_christina-react_app/src/App.js
+++ will_christina-react_app/src/App.js
@@ -6,6 +6,8 @@ import Search from './Search';
 class App extends Component {
 
   state = {
+    bookFilter: '',
+    printFilter: '',
 
   }
 
@@ -19,12 +21,44 @@ class App extends Component {
     const baseUrl = 'https://www.googleapis.com/books/v1/volumes';
     const key = 'AIzaSyAYDJhq65jOJdEWKmAaCQSIUaxDdaMMCf8';
     const fullSearchUrl = this.fullQuery(baseUrl, searchInput, key);
-    fetch()
+    fetch(fullSearchUrl)
 
     
 
   };
 
+  fullQuery = () => {
+    const { bookFilter, printFilter } = this.state;
+    let fullQuery;
+    if ( searchInput !== '') {
+      fullQuery = '?q=' + searchInput;
+    }
+    if ( bookFilter !== '') {
+      fullQuery = fullQuery + '&filter=' + bookFilter; 
+    }
+    if ( printFilter !== '') {
+      fullQuery = fullQuery + '&bookType=' + printFilter;
+    }
+
+    const fullUrl = baseUrl + fullQuery + '&key=' + key;
+    return fullUrl; 
+  }
+
+handlePrintType = ( printType ) => {
+  if ( printType ) {
+    this.setState({
+      printFilter: printType
+    });
+  }
+}
+
+handleBookType = ( bookType ) => {
+  if ( bookType ) {
+    this.setState({
+      bookFilter: bookType
+    });
+  }
+}
 
   render() {
     return (
