93446603b64a62d4aa426453451ba5ef28762df4
Christina Chapman <cmchapman75@gmail.com>
2019-11-18 13:44:53 -0600

    Working on api fetch in App.js

--- will_christina-react_app/src/App.js
+++ will_christina-react_app/src/App.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './Header';
 import Search from './Search';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Search />
-    </div>
-  );
+class App extends Component {
+
+  state = {
+
+  }
+
+
+  handleSubmit = (searchSubmitEvent, searchInput) => {
+    searchSubmitEvent.preventDefault();
+    this.setState({
+      searchQuery: searchInput
+    }); 
+
+    const baseUrl = 'https://www.googleapis.com/books/v1/volumes';
+    const key = 'AIzaSyAYDJhq65jOJdEWKmAaCQSIUaxDdaMMCf8';
+    const fullSearchUrl = this.fullQuery(baseUrl, searchInput, key);
+    fetch()
+
+    
+
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Search />
+      </div>
+    );
+
+  }; 
+  
 }
+
+class 
 
 export default App;
